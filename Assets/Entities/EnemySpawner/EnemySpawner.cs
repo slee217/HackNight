d3cd67a3de94b7b3584cc50c4a4ea0332085b3e0
bluebox159 <bluebox159@gmail.com>
2015-12-01 15:28:15 -0800

    Enemy Spawn
    I got them to be created based on predetermined positions.

--- Assets/Entities/EnemySpawner/EnemySpawner.cs
+++ Assets/Entities/EnemySpawner/EnemySpawner.cs
@@ -6,9 +6,11 @@ public class EnemySpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject enemy = Instantiate(enemyPrefab,new Vector3 (0f,0f,0f),Quaternion.identity) as GameObject;
-		enemy.transform.parent = transform;
-	}
+        foreach(Transform child in transform) {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
