ac7ee3e638a015d23718db69e071696f0b18d433
bluebox159 <bluebox159@gmail.com>
2015-12-01 14:01:19 -0800

    Projectile Movement/Cleaning Up
    Got the thing to shoot projectiles and have them delete when they go off
    screen using the Destroy function.
    
    Also got rid of unnecessary game objects in the first scene, and edited
    build setting so that the Hacknight scene loads first.

--- Assets/Entities/Enemy/EnemySpawner.cs
+++ Assets/Entities/Enemy/EnemySpawner.cs
@@ -12,6 +12,6 @@ public class EnemySpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 }
--- Assets/Entities/Projectiles/Projectile.cs
+++ Assets/Entities/Projectiles/Projectile.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public float speed;
+    public float padding;
+
+    private float xmin;
+    private float xmax;
+
+    // Use this for initialization
+    void Start () {
+
+        speed = 10f;
+        padding = 1f;
+
         transform.rotation = Quaternion.Euler( 0,0,-90 );
-	}
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, distance));
+        xmin = leftmost.x - padding;
+        xmax = rightmost.x + padding;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        MoveProjectile();
 	}
+
+    void MoveProjectile()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        if (transform.position.x >= xmax - padding)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
