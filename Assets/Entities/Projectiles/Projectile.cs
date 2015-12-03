789982f2d8b8964b62f2160902a49e23f2fa8e96
slee217 <slee217@ucr.edu>
2015-12-02 18:33:17 -0800

    this style

--- Assets/Entities/Projectiles/Projectile.cs
+++ Assets/Entities/Projectiles/Projectile.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class Projectile : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-        transform.rotation = Quaternion.Euler( 0,0,-90 );
+	void Start ()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, -90);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        Transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
+    }
 }
